4b548cf95944fbf65ce87941c97803496a7db8d8
PEDRO TIAGO TEIXEIRA PEREIRA <8200431@estg.ipp.pt>
2023-05-28 16:23:56 +0100

    Switched training/testing process to use train.csv for both cases, added more parameters to fitness and a maximum of 5 minutes timer per model training

--- mnist.py
+++ mnist.py
@@ -14,39 +14,50 @@ resources_dir = os.path.join(current_dir, 'resources')
 
 # Load train and test data from Kaggle competition
 train_data = pd.read_csv(os.path.join(resources_dir, 'train.csv'))
-test_data = pd.read_csv(os.path.join(resources_dir, 'test.csv'))
+
+# Split train data into training and testing (80/20)
+train_data_len = len(train_data)
+train_data_split = int(train_data_len * 0.8)
+train_data_train = train_data.iloc[:train_data_split]
+train_data_test = train_data.iloc[train_data_split:]
 
 # Convert data to H2O frames
-train_h2o = h2o.H2OFrame(train_data)
+train_h2o_train = h2o.H2OFrame(train_data_train)
+train_h2o_test = h2o.H2OFrame(train_data_test)
+
+# Load test data
+test_data = pd.read_csv(os.path.join(resources_dir, 'test.csv'))
 test_h2o = h2o.H2OFrame(test_data)
 
 # Set the response column for training purposes
-train_h2o["label"] = train_h2o["label"].asfactor()
+train_h2o_train["label"] = train_h2o_train["label"].asfactor()
 
 # Define three base models
-rf_base = H2ORandomForestEstimator(ntrees=50, max_depth=20, nfolds=5)
-gbm_base = H2OGradientBoostingEstimator(ntrees=50, max_depth=5, nfolds=5)
-dl_base = H2ODeepLearningEstimator(hidden=[100, 100, 100], epochs=10, nfolds=5)
+rf_base = H2ORandomForestEstimator(ntrees=20, max_depth=10, nfolds=5, seed=123, max_runtime_secs=300)
+gbm_base = H2OGradientBoostingEstimator(ntrees=20, max_depth=10, nfolds=5, seed=123, max_runtime_secs=300)
+dl_base = H2ODeepLearningEstimator(hidden=[50, 50], epochs=10, nfolds=5, seed=123, max_runtime_secs=300)
 
 # Add base models to a list
 base_models = [rf_base, gbm_base, dl_base]
 
 # Define genetic algorithm parameters
 # Currently using default values recommended by past studies
-pop_size = 100
-num_generations = 100
+pop_size = 10
+num_generations = 20
 mutation_rate = 0.2
 crossover_rate = 0.85
 
 
 # Define fitness function
 def fitness(model):
-    model.train(x=train_h2o.names[:-1], y="label", training_frame=train_h2o)
-    predictions = model.predict(test_h2o).as_data_frame()['predict'].tolist()
-    label_guess = pd.Series(train_data['label']).mode()[0]
+    model.train(x=train_h2o_train.names[:-1], y="label", training_frame=train_h2o_train)
+    predictions = model.predict(train_h2o_test).as_data_frame()['predict'].tolist()
+    label_guess = pd.Series(train_data_train['label']).mode()[0]
     predictions = [label_guess if np.isnan(x) else x for x in predictions]
-    accuracy = sum(predictions == train_data['label']) / len(train_data)
-    return accuracy
+    correct_predictions = sum(predictions == train_data_test['label'])
+    accuracy = correct_predictions / len(train_data_test)
+    print("Model Accuracy: " + str(accuracy))
+    return accuracy, correct_predictions, predictions
 
 
 # Define genetic operators (crossover and mutation)
@@ -79,7 +90,7 @@ def mutation(model):
 # Initialize population
 population = [np.random.choice(base_models) for _ in range(pop_size)]
 
-best_fitness = 0
+best_accuracy = 0
 best_model = None
 # Evolve population
 for generation in range(num_generations):
@@ -88,31 +99,35 @@ for generation in range(num_generations):
     fitness_scores = [fitness(model) for model in population]
 
     # Select parents for crossover
-    parent1, parent2 = np.random.choice(population, size=2, replace=False, p=fitness_scores/np.sum(fitness_scores))
+    parent1, parent2 = np.random.choice(population, size=2, replace=False,
+                                        p=[score[0] for score in fitness_scores] / np.sum(
+                                            [score[0] for score in fitness_scores]))
 
     # Perform genetic operations
     child1 = crossover(parent1, parent2)
     child2 = mutation(child1)
 
     # Replace least fit model in population with new child
-    least_fit = np.argmin(fitness_scores)
+    least_fit = np.argmin([score[0] for score in fitness_scores])
     population[least_fit] = child2
 
     # Evaluate best fitness and model
-    current_fitness = max(fitness_scores)
-    current_model = population[np.argmax(fitness_scores)]
+    current_accuracy = max([score[0] for score in fitness_scores])
+    current_model = population[np.argmax([score[0] for score in fitness_scores])]
 
     # Check if current fitness is better than the best fitness so far
-    if current_fitness > best_fitness:
-        best_fitness = current_fitness
+    if current_accuracy > best_accuracy:
+        best_accuracy = current_accuracy
         best_model = current_model
 
     # Print progress
-    print("Generation: ", generation, " Best fitness: ", max(fitness_scores))
-
+    print("Generation: ", generation, " Best accuracy: ", max([score[0] for score in fitness_scores]))
 
 # Evaluate best model on test set
-best_model = max(population, key=fitness)
-best_model.train(x=train_h2o.names[:-1], y="label", training_frame=train_h2o)
-test_accuracy = best_model.model_performance(test_data=test_h2o).accuracy()
+best_model_accuracy, _, best_model_predictions = fitness(best_model)
+test_accuracy = sum(best_model_predictions == test_data['label']) / len(test_data)
 print("Test Accuracy: ", test_accuracy)
+
+# Create result.csv containing the ID of the test.csv line and the predicted number ("label")
+result_df = pd.DataFrame({'ID': test_data['ID'], 'label': best_model_predictions})
+result_df.to_csv(os.path.join(resources_dir, 'result.csv'), index=False)
