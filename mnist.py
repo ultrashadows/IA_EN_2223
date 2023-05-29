7e64baeb9871fbd2ac85d63dc332ef24387de8b8
PEDRO TIAGO TEIXEIRA PEREIRA <8200431@estg.ipp.pt>
2023-05-29 15:36:50 +0100

    Added logger files, best model training and saving and attempted first prediction

--- mnist.py
+++ mnist.py
@@ -4,15 +4,31 @@ from h2o.estimators import H2ORandomForestEstimator, H2OGradientBoostingEstimato
 import pandas as pd
 import numpy as np
 import os
+import time
+import logging
 
 # Start H2O
 h2o.init(ip="localhost", port=54321)
 
+# Change default H2O settings for performance
+h2o.no_progress()
+
 # Fetch resources directory
 current_dir = os.path.dirname(__file__)
 resources_dir = os.path.join(current_dir, 'resources')
+results_dir = os.path.join(current_dir, 'results')
+logs_dir = os.path.join(current_dir, 'logs')
 
-# Load train and test data from Kaggle competition
+# Logger config
+current_time_millis = int(round(time.time() * 1000))
+log_file = os.path.join(logs_dir, 'log_' + str(current_time_millis) + ".log")
+logging.basicConfig(filename=log_file,
+                    level=logging.INFO,
+                    format='%(asctime)s %(levelname)s: %(message)s',
+                    datefmt='%d:%m:%Y %H:%M:%S')
+
+# Load train data from Kaggle competition
+logging.info("Importing and handling training and testing data...")
 train_data = pd.read_csv(os.path.join(resources_dir, 'train.csv'))
 
 # Split train data into training and testing (80/20)
@@ -43,7 +59,7 @@ base_models = [rf_base, gbm_base, dl_base]
 # Define genetic algorithm parameters
 # Currently using default values recommended by past studies
 pop_size = 10
-num_generations = 20
+num_generations = 1
 mutation_rate = 0.2
 crossover_rate = 0.85
 
@@ -59,9 +75,11 @@ def fitness(model):
     predictions = model.predict(train_h2o_test).as_data_frame()['predict'].tolist()
     label_guess = pd.Series(train_data_train['label']).mode()[0]
     predictions = [label_guess if np.isnan(x) else x for x in predictions]
-    correct_predictions = sum(predictions == train_data_test['label'])
+    correct_predictions = sum([pred == label for pred, label in zip(predictions, train_data_test['label'])])
     accuracy = correct_predictions / len(train_data_test)
     print("Model Accuracy: {:.2f}%".format(accuracy * 100), " , Models Trained: ", models_trained, "/200")
+    logging.info("Model Accuracy: {:.2f}%".format(accuracy * 100) + " , Models Trained: "
+                 + str(models_trained) + "/200")
     return accuracy, correct_predictions, predictions
 
 
@@ -75,7 +93,8 @@ def crossover(model1, model2):
             if np.random.rand() > 0.5:
                 model1_params[key], model2_params[key] = model2_params[key], model1_params[key]
 
-    new_model = model1.__class__(**model1_params)
+    new_model = model1.__class__()
+    new_model._parms = model1_params
     return new_model
 
 
@@ -88,23 +107,35 @@ def mutation(model):
                 new_val = np.random.choice(model_params[key])
                 model_params[key] = new_val
 
-    new_model = model.__class__(**model_params)
+    new_model = model.__class__()
+    new_model._parms = model_params
     return new_model
 
 
 # Initialize population
 population = [np.random.choice(base_models) for _ in range(pop_size)]
 
+# Initialize best accuracy
 best_accuracy = 0
-best_model = None
+
+# Initialize best model
+best_model_file = os.path.join(resources_dir, 'best_model')
+if os.path.exists(best_model_file):
+    # Load the saved model
+    best_model = h2o.load_model(best_model_file)
+else:
+    # No best model found, create a new one
+    best_model = None
+
 # Evolve population
 for generation in range(num_generations):
+    logging.info("Beginning models training...")
 
     # Evaluate fitness
     fitness_scores = [fitness(model) for model in population]
 
     # Select parents for crossover
-    parent1, parent2 = np.random.choice(population, size=2, replace=False,
+    parent1, parent2 = np.random.choice(population, size=2, replace=True,
                                         p=[score[0] for score in fitness_scores] / np.sum(
                                             [score[0] for score in fitness_scores]))
 
@@ -127,12 +158,26 @@ for generation in range(num_generations):
 
     # Print progress
     print("Generation: ", generation, " Best accuracy: ", max([score[0] for score in fitness_scores]))
+    logging.info("Generation training complete!")
+    logging.info("Generation: " + str(generation) + " Best accuracy: "
+                 + str(max([score[0] for score in fitness_scores])))
 
-# Evaluate best model on test set
-best_model_accuracy, _, best_model_predictions = fitness(best_model)
-test_accuracy = sum(best_model_predictions == test_data['label']) / len(test_data)
-print("Test Accuracy: ", test_accuracy)
+# Train best model on full training data
+logging.info("Best model found, training best model...")
+best_model.train(x=train_h2o_train.names[:-1], y="label", training_frame=train_h2o_train)
+
+# Make predictions on the test data
+logging.info("Predicting test data labels...")
+best_model_predictions = best_model.predict(test_h2o).as_data_frame()['predict'].tolist()
+
+# Save the best model
+logging.info("Saving the best model for future training...")
+best_model.save_model(os.path.join(resources_dir, 'best_model'))
 
 # Create result.csv containing the ID of the test.csv line and the predicted number ("label")
-result_df = pd.DataFrame({'ID': test_data['ID'], 'label': best_model_predictions})
-result_df.to_csv(os.path.join(resources_dir, 'result.csv'), index=False)
+result_df = pd.DataFrame({'ID': test_data['Id'], 'label': best_model_predictions})
+
+# Save result to results folder
+logging.info("Saving results...")
+result_df.to_csv(os.path.join(results_dir, 'predictions_' + str(current_time_millis) + ".csv"), index=False)
+logging.info("Run done!")
